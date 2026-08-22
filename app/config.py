e8405c1c11a280226c7d9d105b00fb5ca7f24192
Pratik5676 <pratikvsalunkhe924@gmail.com>
2026-08-22 23:52:21 +0530

    added comments for better understanding

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-
+# fetches all the api keys and urls from the env file
 class Settings:
     GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
@@ -11,7 +11,7 @@ class Settings:
     QDRANT_COLLECTION = "enterprise_rag"
 
     GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-    GROQ_MODEL = "llama-3.3-70b-versatile"
+    GROQ_MODEL = "openai/gpt-oss-120b"
     GROQ_FALLBACK_API_KEY = os.getenv("GROQ_FALLBACK_API_KEY")
 
 
